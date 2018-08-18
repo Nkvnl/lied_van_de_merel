f4c941001069b4000ccfaaf9f42d0c100a12e8e4
Niek van Losenoord <niek_losenoord@hotmail.com>
2018-08-18 20:41:53 +0000

    second

--- views/script.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-        $('nav').addClass('transparent');
-    }
-    else {
-        $('nav').removeClass('transparent');
-    }
-});
